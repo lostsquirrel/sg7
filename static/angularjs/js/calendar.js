1246ece8e93b1e59cfd928daf907b5c4a0d9e148
lisong <song.li@jnwan.net>
2015-08-29 12:03:22 +0800

    move Calendar  - a route  exercise in codecademy

--- static/angularjs/js/calendar.js
+++ static/angularjs/js/calendar.js
@@ -5,11 +5,11 @@ app.config(['$routeProvider',
     $routeProvider
     .when('/', {
         controller: 'CalendarDayController',
-        templateUrl: '/static/js/views/calendar_day.html'
+        templateUrl: 'js/views/calendar_day.html'
     })
     .when('/:id', {
         controller: 'EventController',
-        templateUrl: '/static/js/views/calendar_event.html'
+        templateUrl: 'js/views/calendar_event.html'
     })
     .otherwise({
         redirectTo: '/'
