ece51f291e62bc37c8750540a775def11424174c
lostsquirrel <songli1988@gmail.com>
2015-09-03 00:37:47 +0800

    add a formula

--- static/js/services/king/services.js
+++ static/js/services/king/services.js
@@ -11,6 +11,27 @@ app.factory('formulas', [ function(){
     },
     function(level) {
         return 18 * level + Math.floor((level - 1) / 3);
+    },
+    function(level) {
+        var x = 0;
+
+        var temp = level;
+        while(temp) {
+            if (temp <= 10) {
+                if (temp % 3 == 0) {
+                    x += 0.01
+                }
+            } else {
+                var y = temp % 10;
+                if (y == 0 || y == 3 || y == 6) {
+                    x += 0.01;
+                }
+            }
+
+            temp--;
+        }
+        var z = 25.93 + 0.39 * (level - 1) + x
+        return z.toFixed(2);
     }
     ];
 
